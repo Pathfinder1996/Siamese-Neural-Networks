fade67f2a1b74131ba58976725245f7f3415b764
Dylan Dai <142645742+Pathfinder1996@users.noreply.github.com>
2024-10-03 04:10:11 +0800

    Add file

---
+++ sub_network.py
@@ -0,0 +1,32 @@
+from keras.layers import *
+from keras.models import *
+
+def sub_network(input_shape, dropout=0.5):
+    c = [32, 64, 128]
+    
+    inputs = Input(shape=input_shape)
+    
+    x = Conv2D(c[0], (3, 3), activation="relu", padding="same")(inputs)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Dropout(dropout)(x)
+
+    x = Conv2D(c[1], (3, 3), activation="relu", padding="same")(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Dropout(dropout)(x)
+
+    x = Conv2D(c[2], (3, 3), activation="relu", padding="same")(x)
+    x = BatchNormalization()(x)
+    x = MaxPooling2D((2, 2))(x)
+    x = Dropout(dropout)(x)
+    
+    x = GlobalAveragePooling2D()(x)
+    
+    x = Dense(128)(x)
+
+    model = Model(inputs=inputs, outputs=x)
+    
+    model.summary()
+
+    return model
